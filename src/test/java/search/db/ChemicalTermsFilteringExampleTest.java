823e704460e228cdb99c1c7af91cdfe192be62cf
Robert Wagner <rwagner@chemaxon.com>
2021-01-28 11:00:58 +0100

    relax test so we won't have to change it always

--- src/test/java/search/db/ChemicalTermsFilteringExampleTest.java
+++ src/test/java/search/db/ChemicalTermsFilteringExampleTest.java
@@ -1,6 +1,9 @@
 package search.db;
 
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.hamcrest.Matchers;
 import org.junit.After;
@@ -21,10 +24,21 @@ public class ChemicalTermsFilteringExampleTest {
 	@Test
 	public void chemicalTermsFilteringTest() {
 		ChemicalTermsFilteringExample.main(null);
-		assertThat(pc.getOutputLines(), Matchers.hasItems("Search has found 121 hits in which O has pka value greater than 2", "Search has found 81 hits in which O has pka value greater than 3.5"));
+		
+		List<String> outputLines = pc.getOutputLines();
+		assertTrue(getHitAndPkaAboveLimit(outputLines.get(0))>60);
+        assertTrue(getHitAndPkaAboveLimit(outputLines.get(1))>60);
+		
+        assertThat(outputLines, Matchers.hasItems("Search has found 121 hits in which O has pka value greater than 2", "Search has found 81 hits in which O has pka value greater than 3.5"));
 	}
 
-	@After
+	private int getHitAndPkaAboveLimit(String string) {
+	    String hitCountStr = 
+	            string.replaceFirst("Search has found ", "").replaceFirst(" hits in which .* has pka value greater than .*", "");
+        return Integer.valueOf( hitCountStr);
+    }
+
+    @After
 	public void resetOutputStream() {
 		ChemicalTermsFilteringExample.out = System.out;
 	}
