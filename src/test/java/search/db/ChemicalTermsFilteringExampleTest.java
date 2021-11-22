87f66a07ad314de79841d76ccd842477d457ca75
Robert Wagner <rwagner@chemaxon.com>
2021-11-22 15:53:30 +0100

    pKa may vary from release to release

--- src/test/java/search/db/ChemicalTermsFilteringExampleTest.java
+++ src/test/java/search/db/ChemicalTermsFilteringExampleTest.java
@@ -28,8 +28,6 @@ public class ChemicalTermsFilteringExampleTest {
 		List<String> outputLines = pc.getOutputLines();
 		assertTrue(getHitAndPkaAboveLimit(outputLines.get(0))>60);
         assertTrue(getHitAndPkaAboveLimit(outputLines.get(1))>60);
-		
-        assertThat(outputLines, Matchers.hasItems("Search has found 121 hits in which O has pka value greater than 2", "Search has found 81 hits in which O has pka value greater than 3.5"));
 	}
 
 	private int getHitAndPkaAboveLimit(String string) {
