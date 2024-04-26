c1abfacdaebcd9b84b22dea8da00287e71730501
Tamás Varjas <tvarjas@chemaxon.com>
2024-04-26 11:40:53 +0200

    JCB-24962: pka hit count down to 6 with CCL version 24.3.0-13750

--- src/test/java/search/db/CalculatedColumnsSearchExampleTest.java
+++ src/test/java/search/db/CalculatedColumnsSearchExampleTest.java
@@ -12,37 +12,37 @@ import com.chemaxon.test.helper.PrintCollector;
 
 public class CalculatedColumnsSearchExampleTest {
 
-	private PrintCollector pc = new PrintCollector();
-	
+	private final PrintCollector pc = new PrintCollector();
+
 	@Before
 	public void setOutputStream() {
 		CalculatedColumnsSearchExample.out=pc.getOutStream();
 	}
-	
+
 	@Test
 	public void logPHas69Hits() {
 		CalculatedColumnsSearchExample.main(new String[] {});
 		assertThat(pc.getOutputLines().get(2), Matchers.is("Results using logp"));
 		assertThat(pc.getOutputLines().get(3), Matchers.is("Hit count: 69"));
 	}
-	
+
 	@Test
 	public void rtblBndCountHas0Hits() {
 		CalculatedColumnsSearchExample.main(new String[] {});
 		assertThat(pc.getOutputLines().get(6), Matchers.is("Results using rtbl_bnd_cnt"));
 		assertThat(pc.getOutputLines().get(7), Matchers.is("Hit count: 0"));
 	}
-	
+
 	@Test
-	public void pkaAc2HasAtLeast9Hits() {
+	public void pkaAc2HasAtLeast6Hits() {
 		CalculatedColumnsSearchExample.main(new String[] {});
 		assertThat(pc.getOutputLines().get(10), Matchers.is("Results using pka_ac_2"));
-		assertTrue(Integer.parseInt(pc.getOutputLines().get(11).substring("Hit count: ".length()))>8);
+		assertTrue(Integer.parseInt(pc.getOutputLines().get(11).substring("Hit count: ".length()))>=6);
 	}
-	
+
 	@After
 	public void resetOutputStream() {
 		CalculatedColumnsSearchExample.out=System.out;
 	}
-	
+
 }
