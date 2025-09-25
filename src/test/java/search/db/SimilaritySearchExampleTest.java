fa4dfa4fa08000e8d9dd8c3766554d695de3dbf0
Tamás Varjas <tvarjas@chemaxon.com>
2025-09-25 16:55:52 +0200

    JCB-25048: Add asserts to SimilaritySearchExampleTest

--- src/test/java/search/db/SimilaritySearchExampleTest.java
+++ src/test/java/search/db/SimilaritySearchExampleTest.java
@@ -1,13 +1,14 @@
 package search.db;
 
+import static org.junit.Assert.assertThat;
+
+import org.hamcrest.Matchers;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import com.chemaxon.test.helper.PrintCollector;
 
-@Ignore
 public class SimilaritySearchExampleTest {
 	private PrintCollector pc = new PrintCollector();
 	
@@ -17,11 +18,34 @@ public class SimilaritySearchExampleTest {
 	}
 	
 	@Test
-	public void search() {
+	public void chemicalHashedFingerprintHas10Hits() {
 		SimilaritySearchExample.main(null);
-	}
-	
-	@After
+        assertThat(pc.getOutputLines().get(1), Matchers.is("Results using chemical hashed fingerprint:"));
+        assertThat(pc.getOutputLines().get(2), Matchers.is("Hit count: 10"));
+    }
+
+    @Test
+    public void pharmacophoreHas130Hits() {
+        SimilaritySearchExample.main(null);
+        assertThat(pc.getOutputLines().get(5), Matchers.is("Results using descriptor: Pharmacophore"));
+        assertThat(pc.getOutputLines().get(6), Matchers.is("Hit count: 130"));
+    }
+
+    @Test
+    public void hDonorHas382Hits() {
+        SimilaritySearchExample.main(null);
+        assertThat(pc.getOutputLines().get(9), Matchers.is("Results using descriptor: HDonor"));
+        assertThat(pc.getOutputLines().get(10), Matchers.is("Hit count: 382"));
+    }
+
+    @Test
+    public void hAcceptorHas382Hits() {
+        SimilaritySearchExample.main(null);
+        assertThat(pc.getOutputLines().get(13), Matchers.is("Results using descriptor: HAcceptor"));
+        assertThat(pc.getOutputLines().get(14), Matchers.is("Hit count: 382"));
+    }
+
+    @After
 	public void resetOutputStream() {
 		SimilaritySearchExample.out=System.out;
 	}
